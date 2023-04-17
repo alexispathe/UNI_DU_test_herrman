3a2e8f7720969cd9ac8d2fd3289202efcaed44a4
Alexis Pathe <alexispatheguillen@gmail.com>
2023-04-16 23:00:06 -0600

    Se termina de dar la logica del proyecto

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const getQuestions =()=>{
+const getQuestions = () => {
     const container = document.querySelector('.container');
     // En esta funcion vamos a devolver todas las preguntas para mostrarlas en la pagina
     const preguntas = [
         {
-            question :'Cuando un buen amigo, pareja o familiar acude a mí porque tiene un problema...',
-            options :[
+            question: 'Cuando un buen amigo, pareja o familiar acude a mí porque tiene un problema...',
+            options: [
                 'No me preocupo si el problema no es considerablemente grave',
                 'Le escribo una lista con las posibles soluciones',
                 'Abrazo a esa persona y lo siento mucho por ella',
@@ -12,8 +12,8 @@ const getQuestions =()=>{
             ]
         },
         {
-            question :'En el momento de estudiar antes de hacer un examen...',
-            options :[
+            question: 'En el momento de estudiar antes de hacer un examen...',
+            options: [
                 'Disfruto mucho haciendo ejercicios para prepararme el examen',
                 'Elaboro un plan de estudio, compaginando descanso con horas de concentración',
                 'Me pongo muy nervioso/a y siento mucha ansiedad, sobre todo el día antes',
@@ -21,8 +21,8 @@ const getQuestions =()=>{
             ]
         },
         {
-            question :'En una pareja lo que busco es...',
-            options :[
+            question: 'En una pareja lo que busco es...',
+            options: [
                 'Una persona con la que compartir conocimientos y sabiduría',
                 'Busco a alguien compatible con mi personalidad, agradable y que yo considere atractivo/a',
                 'Amor y pasión, me gusta sentir muchas emociones en mi relación de pareja',
@@ -30,17 +30,17 @@ const getQuestions =()=>{
             ]
         },
         {
-            question :'Cuando tengo una discusión familiar...',
-            options :[
-               ' No me involucro demasiado, solamente observo cómo se comportan los demás',
+            question: 'Cuando tengo una discusión familiar...',
+            options: [
+                ' No me involucro demasiado, solamente observo cómo se comportan los demás',
                 'Analizo cómo se porta cada miembro de la familia e intento actuar en consecuencia',
                 'Me pongo a llorar, no soporto ver como mi familia se pelea, son personas que quiero mucho',
                 'Busco la mejor manera de calmar el ambiente y me invento alguna dinámica o juego si hace falta'
             ]
         },
         {
-            question :'Cuando tengo un presentimiento o una corazonada...',
-            options :[
+            question: 'Cuando tengo un presentimiento o una corazonada...',
+            options: [
                 'No suelo tener ese tipo de sensaciones',
                 'Pienso por qué lo siento e intento buscar la explicación más razonable de dicho presentimiento',
                 'Me alegro mucho, me encanta sentir cómo mi mente me intenta decir algo',
@@ -48,50 +48,83 @@ const getQuestions =()=>{
             ]
         },
         {
-            question :'Mi trabajo ideal sería...',
-            options :[
+            question: 'Mi trabajo ideal sería...',
+            options: [
                 'Profesor/a de matemáticas, física o empresario/a',
                 'Administrativo/a, contable o gerente',
                 'Psicólogo/a, periodista o trabajador/a social',
                 'Arquitecto, compositor/a, escritor/a, poeta o diseñador/a'
             ]
         }
-       
+
     ];
-    // Crearemos una etiqueta contenedora para mostrar las preguntas
-    const questionContainer = document.createElement('form');
-    questionContainer.className = 'questionContainer';
-    // Crearemos una etiqueta select para guardar las opciones 
-    const question = document.createElement('section');
-    
-    
-    preguntas.map((pregunta, i)=>{
-        const h1 = document.createElement('h1')
+    // Llamamos a la  etiqueta contenedora de las preguntas
+    const question = document.querySelector('.question-container');
+    // Creamos el boton de submit para que se envien los datos
+    const btnSubmit = document.createElement('button');
+    btnSubmit.className = "btn-submit";
+    btnSubmit.textContent = "Ver resultados"
+    preguntas.map((pregunta, i) => {
+
+        // Esta etiqueta servira para almacenar cada pregunta 
+        const formCheck = document.createElement('div');
+        formCheck.className = 'form-check';
+        // Crearemos un h1 para guardar el titulo de la pregunta 
+        const h1 = document.createElement('h1');
         h1.textContent = pregunta.question;
-        question.append(h1);
-        // Crearemos una etiqueta contenedora para cada opcion de respuesta
-        pregunta.options.map((option, j)=>{
-        const optionContainer = document.createElement('option');
-        optionContainer.className = 'option';
-        // Creamos un parrafo para mostrar la pregunta 
-        const p = document.createElement('p');
-        // console.log(option)
-        p.textContent = `${j+1}- ${option}`;
-            optionContainer.append(p);
-            question.append(optionContainer);
+        formCheck.append(h1);
+        pregunta.options.map((option, j) => {
+            // Crearemos un input para almacenar las opciones
+            const input = document.createElement('input');
+            input.className = 'form-check-input';
+            input.type = "radio";
+            input.value = j + 1;
+            input.name = `question-${i}`
+            // Crearemos un label para guardar el nombre de la opcion
+            const label = document.createElement('label');
+            label.textContent = option;
+            label.className = 'form-check-label';
+            formCheck.append(input);
+            formCheck.append(label);
         })
-        questionContainer.append(question)
+        question.append(formCheck)
+
     })
-    container.append(questionContainer);
-    handleClick();
-    
+    question.append(btnSubmit)
+    container.append(question)
+
+    handleClick(preguntas.length);
+
 }
-const handleClick =()=>{
-    const options = document.querySelectorAll('.option');
-    options.forEach((option, i)=>{
-        option.addEventListener('click',()=>{
-            console.log(i+1)
-        });
-    })
+const handleClick = (num) => {
+    /* Con esta funcion vamos a capturar las respuestas y hacer la suma correspondiente*/
+
+    const btnClick = document.querySelector('.btn-submit');
+    btnClick.addEventListener('click', () => {
+        let value = 0; //Esta constante permitira guardar cada valor de la respuesta
+        for (let i = 0; i < num; i++) {
+            document.getElementsByName(`question-${i}`).forEach((r, i) => {
+                if (r.checked) {
+                    value += parseInt(r.value);
+                }
+            })
+        }
+        result(value);
+    });
+}
+const result = (valor) => {
+    /*Esta funcion permitira hacer la validacion para saber que Cuadrante predomina la persona*/
+    const result = document.querySelector('.result');
+    const questionContainer = document.querySelector('.question-container');
+    if (valor >= 0 && valor <= 6) {
+        result.textContent = 'Cuadrante predominante A (Lógico-matemático)'
+    } else if (valor >= 6 && valor <= 12) {
+        result.textContent = 'Cuadrante predominante B (Organizado-Analista)'
+    } else if (valor >= 12 && valor <= 18) {
+        result.textContent = 'Cuadrante predominante C (Emocional-Sensitivo)'
+    } else if (valor >= 18 && valor <= 24) {
+        result.textContent = 'Cuadrante predominante D (Intuitivo-Imaginativo)'
+    }
+
 }
 getQuestions();
