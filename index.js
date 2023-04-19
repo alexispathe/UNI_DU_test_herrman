1160d04fded0f574d70922161b21980ea8d71959
Alexis Pathe <alexispatheguillen@gmail.com>
2023-04-18 23:30:51 -0600

    Se separa los datos en un archivo externo

--- index.js
+++ index.js
@@ -1,63 +1,8 @@
+import {preguntas} from './questionsDB.js'; //Devolvemos las preguntas
 const getQuestions = () => {
     const container = document.querySelector('.container');
     // En esta funcion vamos a devolver todas las preguntas para mostrarlas en la pagina
-    const preguntas = [
-        {
-            question: 'Cuando un buen amigo, pareja o familiar acude a mí porque tiene un problema...',
-            options: [
-                'No me preocupo si el problema no es considerablemente grave',
-                'Le escribo una lista con las posibles soluciones',
-                'Abrazo a esa persona y lo siento mucho por ella',
-                'Me invento un plan para ayudarla'
-            ]
-        },
-        {
-            question: 'En el momento de estudiar antes de hacer un examen...',
-            options: [
-                'Disfruto mucho haciendo ejercicios para prepararme el examen',
-                'Elaboro un plan de estudio, compaginando descanso con horas de concentración',
-                'Me pongo muy nervioso/a y siento mucha ansiedad, sobre todo el día antes',
-                'Busco apuntes divertidos, hago dibujos para acordarme y elaboro técnicas para recordar nombres y fechas',
-            ]
-        },
-        {
-            question: 'En una pareja lo que busco es...',
-            options: [
-                'Una persona con la que compartir conocimientos y sabiduría',
-                'Busco a alguien compatible con mi personalidad, agradable y que yo considere atractivo/a',
-                'Amor y pasión, me gusta sentir muchas emociones en mi relación de pareja',
-                'Alguien con quien compartir aventuras y nuevas experiencias',
-            ]
-        },
-        {
-            question: 'Cuando tengo una discusión familiar...',
-            options: [
-                ' No me involucro demasiado, solamente observo cómo se comportan los demás',
-                'Analizo cómo se porta cada miembro de la familia e intento actuar en consecuencia',
-                'Me pongo a llorar, no soporto ver como mi familia se pelea, son personas que quiero mucho',
-                'Busco la mejor manera de calmar el ambiente y me invento alguna dinámica o juego si hace falta'
-            ]
-        },
-        {
-            question: 'Cuando tengo un presentimiento o una corazonada...',
-            options: [
-                'No suelo tener ese tipo de sensaciones',
-                'Pienso por qué lo siento e intento buscar la explicación más razonable de dicho presentimiento',
-                'Me alegro mucho, me encanta sentir cómo mi mente me intenta decir algo',
-                'Sigo esa corazonada, no suelo equivocarme nunca',
-            ]
-        },
-        {
-            question: 'Mi trabajo ideal sería...',
-            options: [
-                'Profesor/a de matemáticas, física o empresario/a',
-                'Administrativo/a, contable o gerente',
-                'Psicólogo/a, periodista o trabajador/a social',
-                'Arquitecto, compositor/a, escritor/a, poeta o diseñador/a'
-            ]
-        }
-
-    ];
+   
     // Llamamos a la  etiqueta contenedora de las preguntas
     const question = document.querySelector('.question-container');
     // Creamos el boton de submit para que se envien los datos
